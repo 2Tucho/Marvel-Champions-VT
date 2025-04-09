02ae88eb8a722f43edea83b88adce8c7c33680ef
2Tucho <albergromero@gmail.com>
2025-04-09 11:39:16 +0200

    Added villain encounter components to List

--- marvel-champions-vt/src/components/Main/List/List.jsx
+++ marvel-champions-vt/src/components/Main/List/List.jsx
@@ -1,8 +1,38 @@
 import React from "react";
+import CoreSet from "./CoreSet";
+import TheGreenGoblin from "./TheGreenGoblin";
+import TheWreckingCrew from "./TheWreckingCrew";
+import TheRiseOfRedSkull from "./TheRiseOfRedSkull";
+import TheOnceAndFutureKang from "./TheOnceAndFutureKang";
+import GalaxysMostWanted from "./GalaxysMostWanted";
+import TheMadTitansShadow from "./TheMadTitansShadow";
+import TheHood from "./TheHood";
+import SinisterMotives from "./SinisterMotives";
+import MutantGenesis from "./MutantGenesis";
+import MojoMania from "./MojoMania";
+import NextEvolution from "./NextEvolution";
+import TheAgeOfApocalypse from "./TheAgeOfApocalypse";
+import AgentsOfShield from "./AgentsOfShield";
 
 function List()  {
   
-  return <div>List</div>;
+  return <div>
+    <h3>List</h3>
+    <CoreSet/>
+    <TheGreenGoblin/>
+    <TheWreckingCrew/>
+    <TheRiseOfRedSkull/>
+    <TheOnceAndFutureKang/>
+    <GalaxysMostWanted/>
+    <TheMadTitansShadow/>
+    <TheHood/>
+    <SinisterMotives/>
+    <MutantGenesis/>
+    <MojoMania/>
+    <NextEvolution/>
+    <TheAgeOfApocalypse/>
+    <AgentsOfShield/>
+    </div>;
 
 }
 
