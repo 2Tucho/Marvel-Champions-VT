d779fdde9ddae4f64b5e0101457cffe3584a7c6f
2Tucho <albergromero@gmail.com>
2025-04-09 12:30:02 +0200

    Added mixins.scss in Utils

--- marvel-champions-vt/src/components/Main/List/CoreSet/CoreSet.jsx
+++ marvel-champions-vt/src/components/Main/List/CoreSet/CoreSet.jsx
@@ -3,10 +3,12 @@ import React from "react";
 function CoreSet() {
 
   return <article id="coreset">
+
     <h4>CORE SET</h4>
     <h5>Rhyno</h5>
     <h5>Klaw</h5>
     <h5>Ultron</h5>
+    
     </article>;
 
 }
