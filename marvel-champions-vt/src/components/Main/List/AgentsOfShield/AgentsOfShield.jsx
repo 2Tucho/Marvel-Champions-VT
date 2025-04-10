885e9f370bbb2ade668cbe7bdb09c2c75f444e0e
2Tucho <albergromero@gmail.com>
2025-04-10 11:33:06 +0200

    Added basic AgentsOfShield component estructure and style

--- marvel-champions-vt/src/components/Main/List/AgentsOfShield/AgentsOfShield.jsx
+++ marvel-champions-vt/src/components/Main/List/AgentsOfShield/AgentsOfShield.jsx
@@ -1,9 +1,20 @@
-import React, { Component } from "react";
+import React from "react";
 
-class AgentsOfShield extends Component {
-  render() {
-    return <div>AgentsOfShield</div>;
-  }
+function AgentsOfShield() {
+
+  return <article id="agentsOfShield">
+
+    <h4 className="villainEncounterName">AGENTS OF S.H.I.E.L.D.</h4>
+    <ul className="villainList campaignSet2">
+      <h5>Black Widow</h5>
+      <h5>Batroc</h5>
+      <h5>M.O.D.O.K.</h5>
+      <h5>Thunderbolts</h5>
+      <h5>Baron Zemo</h5>
+    </ul>
+    
+    </article>;
+
 }
 
 export default AgentsOfShield;
