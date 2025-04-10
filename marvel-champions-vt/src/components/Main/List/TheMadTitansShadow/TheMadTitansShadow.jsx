a78940cfe8ff4f0c635583a5b29fd164eee0ecf8
2Tucho <albergromero@gmail.com>
2025-04-10 11:00:55 +0200

    Added basic TheMadTitansShadow component estructure and style

--- marvel-champions-vt/src/components/Main/List/TheMadTitansShadow/TheMadTitansShadow.jsx
+++ marvel-champions-vt/src/components/Main/List/TheMadTitansShadow/TheMadTitansShadow.jsx
@@ -1,9 +1,20 @@
-import React, { Component } from "react";
+import React from "react";
 
-class TheMadTitansShadow extends Component {
-  render() {
-    return <div>TheMadTitansShadow</div>;
-  }
+function TheMadTitansShadow() {
+
+  return <article id="theMadTitansShadow">
+
+    <h4 className="villainEncounterName">THE MAD TITAN'S SHADOW</h4>
+    <ul className="villainList tmts">
+      <h5>Ebony Maw</h5>
+      <h5>Tower Defense</h5>
+      <h5>Thanos</h5>
+      <h5>Hela</h5>
+      <h5>Loki</h5>
+    </ul>
+    
+    </article>;
+
 }
 
 export default TheMadTitansShadow;
