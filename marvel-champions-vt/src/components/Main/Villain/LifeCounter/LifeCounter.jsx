ed6069065b4a7a7dd2fc0f62ef1344b11262eaef
2Tucho <albergromero@gmail.com>
2025-04-11 10:39:39 +0200

    Added plus and minus buttons to LifeCounter

--- marvel-champions-vt/src/components/Main/Villain/LifeCounter/LifeCounter.jsx
+++ marvel-champions-vt/src/components/Main/Villain/LifeCounter/LifeCounter.jsx
@@ -1,8 +1,20 @@
 import React from "react";
+import { useState } from "react";
 
 function LifeCounter() {
 
-  return <div>LifeCounter</div>;
+  const [count, setCount] = useState(0)
+
+  return <div id="lifeCounter">
+      <p>HP:</p>
+      <p>{count}</p>
+      <button onClick={() => setCount((count) => count + 1)}>
+        +
+      </button>
+      <button onClick={() => setCount((count) => count - 1)}>
+        -
+      </button>
+  </div>;
 
 }
 
