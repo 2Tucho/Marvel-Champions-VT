a1052cadc3bd98888f64b8466d017cf8162e1bc9
2Tucho <albergromero@gmail.com>
2025-04-14 10:53:56 +0200

    Add status cards buttons and true-false logic

--- marvel-champions-vt/src/components/Main/Villain/StatusCards/StatusCards.jsx
+++ marvel-champions-vt/src/components/Main/Villain/StatusCards/StatusCards.jsx
@@ -1,9 +1,34 @@
-import React, { Component } from "react";
+import React from "react";
+import {useState} from "react";
 
-class StatusCards extends Component {
-  render() {
-    return <div>StatusCards</div>;
+function StatusCards () {
+  // States to keep track of the active Status Cards on the Villain
+  const [tough, setTough] = useState(false); 
+  const [confused, setConfused] = useState(false); 
+  const [stunned, setStunned] = useState(false);
+
+  function changeStatusCard (status) {
+    switch (status) {
+      case "tough":
+        setTough(!tough);
+        break;
+      case "confused":
+        setConfused(!confused);
+        break;
+      case "stunned":
+        setStunned(!stunned);
+        break;
+      default:
+        break;
+    }
   }
+
+  return <article id="statusCards">
+    <button id="phaseI" onClick={() => changeStatusCard("tough")}>Tough</button> 
+    <button id="phaseII" onClick={() => changeStatusCard("confused")}>Confused</button>
+    <button id="phaseII" onClick={() => changeStatusCard("stunned")}>Stunned</button>
+  </article>;
+
 }
 
 export default StatusCards;
