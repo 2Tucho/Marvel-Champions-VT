1881403dca979cd51ad280e2e49caa7b6b41530f
2Tucho <albergromero@gmail.com>
2025-04-16 11:09:12 +0200

    Added Stelwart button logic

--- marvel-champions-vt/src/components/Main/Villain/StatusCards/StatusCards.jsx
+++ marvel-champions-vt/src/components/Main/Villain/StatusCards/StatusCards.jsx
@@ -6,6 +6,8 @@ function StatusCards ({setToughStatus, setConfusedStatus, setStunnedStatus}) {
   const [tough, setTough] = useState(false); 
   const [confused, setConfused] = useState(false); 
   const [stunned, setStunned] = useState(false);
+  const [hideStatus, setHideStatus] = useState(false); // New state to hide buttons
+
 
   const changeStatusCard = (status) =>{
     switch (status) {
@@ -31,21 +33,45 @@ function StatusCards ({setToughStatus, setConfusedStatus, setStunnedStatus}) {
   setConfusedStatus(confused);
   setStunnedStatus(stunned);
 
+  const hideConfusedAndStunned = () => {
+    setHideStatus(!hideStatus); // Set hideStatus to true to hide the buttons
+    /* Set confused and stunned to false to remove the status */
+    setConfused(false);
+    setStunned(false); 
+  };
+
   return <div>
     <article>
-      <button>Stelwart</button>
+      <button onClick={() => hideConfusedAndStunned()}>Stelwart</button>
       <button>Steady</button>
     </article>
+
     <article id="statusCards">
       <button id="toughStatus" onClick={() => changeStatusCard("tough")} style={{
             backgroundColor: tough ? "#FFB23B" : "#FFE0B2", // Darker orange if active, lighter orange if inactive
           }}>Tough</button>
-      <button id="confusedStatus" onClick={() => changeStatusCard("confused")} style={{
-            backgroundColor: confused ? "#EE7AFF" : "#F8C2F6", // Darker orange if active, lighter orange if inactive
-          }}>Confused</button>
-      <button id="stunnedStatus" onClick={() => changeStatusCard("stunned")} style={{
-            backgroundColor: stunned ? "#9CFF7A" : "#C6EEB8", // Darker red if active, lighter red if inactive
-          }}>Stunned</button>
+        {!hideStatus && (
+          <button
+            id="confusedStatus"
+            onClick={() => changeStatusCard("confused")}
+            style={{
+              backgroundColor: confused ? "#EE7AFF" : "#F8C2F6", // Darker orange if active, lighter orange if inactive
+            }}
+          >
+            Confused
+          </button>
+        )}
+        {!hideStatus && (
+          <button
+            id="stunnedStatus"
+            onClick={() => changeStatusCard("stunned")}
+            style={{
+              backgroundColor: stunned ? "#9CFF7A" : "#C6EEB8", // Darker red if active, lighter red if inactive
+            }}
+          >
+            Stunned
+          </button>
+        )}
     </article>;
   </div>
 }
