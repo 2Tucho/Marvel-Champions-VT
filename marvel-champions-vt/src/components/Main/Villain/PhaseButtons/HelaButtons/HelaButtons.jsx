f20c53156829800e33b7fe6f84e795c773ebd341
2Tucho <albergromero@gmail.com>
2025-04-21 14:17:43 +0200

    Add basic logic and styles to HelaButtons

--- marvel-champions-vt/src/components/Main/Villain/PhaseButtons/HelaButtons/HelaButtons.jsx
+++ marvel-champions-vt/src/components/Main/Villain/PhaseButtons/HelaButtons/HelaButtons.jsx
@@ -1,9 +1,40 @@
-import React, { Component } from "react";
+import React from "react";
+import { useContext, useState } from "react";
+import { PhaseButtonsContext } from "../../../../../context/PhaseButtonsContext";
 
-class HelaButtons extends Component {
-  render() {
-    return <div>HelaButtons</div>;
-  }
+function HelaButtons() { 
+  const {setPhase} = useContext(PhaseButtonsContext);
+
+  /* State to change the buttons depending the selected mode */
+  const [mode, setMode] = useState("normal");
+  /* Function to change the mode of the buttons */
+  const changeMode = (newMode) => {newMode === "normal" ? setMode("normal") : setMode("expert")}
+
+
+
+  return <div id="helaButtons">
+
+    <article id="helaModeButtons">
+      <button onClick={() => changeMode("normal")}>Normal</button>
+      <button onClick={() => changeMode("expert")}>Expert</button>
+    </article>
+
+    {mode === "normal" && (
+      <article>
+        <button onClick={() => setPhase("default")}>Mystic</button>
+        <button onClick={() => setPhase("A2")}>Wounded</button>
+      </article>
+    )}
+
+    {mode === "expert" && (
+      <article>
+        <button onClick={() => setPhase("B1")}>Mystic</button>
+        <button onClick={() => setPhase("B2")}>Wounded</button>
+      </article>
+    )}
+
+  </div>;
+
 }
 
 export default HelaButtons;
