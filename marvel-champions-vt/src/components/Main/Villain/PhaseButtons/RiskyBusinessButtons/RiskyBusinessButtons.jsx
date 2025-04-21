aac43617b9716bd576d45039b76800f1ba1c96e5
2Tucho <albergromero@gmail.com>
2025-04-21 13:29:50 +0200

    Added simple style to Risky Business buttons

--- marvel-champions-vt/src/components/Main/Villain/PhaseButtons/RiskyBusinessButtons/RiskyBusinessButtons.jsx
+++ marvel-champions-vt/src/components/Main/Villain/PhaseButtons/RiskyBusinessButtons/RiskyBusinessButtons.jsx
@@ -5,27 +5,23 @@ import { PhaseButtonsContext } from "../../../../../context/PhaseButtonsContext"
 function RiskyBusinessButtons() { 
   const {setPhase} = useContext(PhaseButtonsContext);
 
-  return <div>
-    
-    <div>
+  return <div id="riskyBusinessButtons">
 
-      {/*  setPhase is wrapped in a function so that they are only executed when the buttons are clicked, not during the render phase*/}
-      <article>
-        <button className="phaseIA" onClick={() => setPhase("default")}>Norman Osborn I</button> 
-        <button className="phaseIB" onClick={() => setPhase("phaseIB")}>Green Goblin I</button>
-      </article>
+    {/*  setPhase is wrapped in a function so that they are only executed when the buttons are clicked, not during the render phase*/}
+    <article>
+      <button className="phaseIA" onClick={() => setPhase("default")}>Norman Osborn I</button> 
+      <button className="phaseIB" onClick={() => setPhase("phaseIB")}>Green Goblin I</button>
+    </article>
 
-      <article>
-        <button className="phaseIIA" onClick={() => setPhase("phaseIIA")}>Norman Osborn II</button> 
-        <button className="phaseIIB" onClick={() => setPhase("phaseIIB")}>Green Goblin II</button>
-      </article>
+    <article>
+      <button className="phaseIIA" onClick={() => setPhase("phaseIIA")}>Norman Osborn II</button> 
+      <button className="phaseIIB" onClick={() => setPhase("phaseIIB")}>Green Goblin II</button>
+    </article>
 
-      <article>
-        <button className="phaseIIIA" onClick={() => setPhase("phaseIIIA")}>Norman Osborn III</button> 
-        <button className="phaseIIIB" onClick={() => setPhase("phaseIIIB")}>Green Goblin III</button>
-      </article>
-
-    </div>
+    <article>
+      <button className="phaseIIIA" onClick={() => setPhase("phaseIIIA")}>Norman Osborn III</button> 
+      <button className="phaseIIIB" onClick={() => setPhase("phaseIIIB")}>Green Goblin III</button>
+    </article>
 
   </div>;
 
