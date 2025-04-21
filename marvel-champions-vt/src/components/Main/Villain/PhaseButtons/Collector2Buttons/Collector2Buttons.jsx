a98cd06fddc8c0da89819cf8354fe9ff9292bfaf
2Tucho <albergromero@gmail.com>
2025-04-21 14:12:58 +0200

    Add basic logic and styles to Collector2Buttons

--- marvel-champions-vt/src/components/Main/Villain/PhaseButtons/Collector2Buttons/Collector2Buttons.jsx
+++ marvel-champions-vt/src/components/Main/Villain/PhaseButtons/Collector2Buttons/Collector2Buttons.jsx
@@ -1,9 +1,40 @@
-import React, { Component } from "react";
+import React from "react";
+import { useContext, useState } from "react";
+import { PhaseButtonsContext } from "../../../../../context/PhaseButtonsContext";
 
-class Collector2Buttons extends Component {
-  render() {
-    return <div>Collector2Buttons</div>;
-  }
+function Collector2Buttons() { 
+  const {setPhase} = useContext(PhaseButtonsContext);
+
+  /* State to change the buttons depending the selected mode */
+  const [mode, setMode] = useState("normal");
+  /* Function to change the mode of the buttons */
+  const changeMode = (newMode) => {newMode === "normal" ? setMode("normal") : setMode("expert")}
+
+
+
+  return <div id="collector2Buttons">
+
+    <article id="collectro2ModeButtons">
+      <button onClick={() => changeMode("normal")}>Normal</button>
+      <button onClick={() => changeMode("expert")}>Expert</button>
+    </article>
+
+    {mode === "normal" && (
+      <article>
+        <button onClick={() => setPhase("default")}>Elder</button>
+        <button onClick={() => setPhase("A2")}>Wounded</button>
+      </article>
+    )}
+
+    {mode === "expert" && (
+      <article>
+        <button onClick={() => setPhase("B1")}>Elder</button>
+        <button onClick={() => setPhase("B2")}>Wounded</button>
+      </article>
+    )}
+
+  </div>;
+
 }
 
 export default Collector2Buttons;
