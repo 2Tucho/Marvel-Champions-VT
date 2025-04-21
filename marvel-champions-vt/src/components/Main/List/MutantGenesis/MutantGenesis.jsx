1f6d63b8a403ebe4ccfebc1ece61bccad215c4d2
2Tucho <albergromero@gmail.com>
2025-04-21 10:22:33 +0200

    Added Mutant Genesis routes

--- marvel-champions-vt/src/components/Main/List/MutantGenesis/MutantGenesis.jsx
+++ marvel-champions-vt/src/components/Main/List/MutantGenesis/MutantGenesis.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 function MutantGenesis() {
 
@@ -6,11 +7,11 @@ function MutantGenesis() {
 
     <h4 className="villainEncounterName">Mutant Genesis</h4>
     <ul className="villainList campaignSet2">
-      <h5>Sabretooth</h5>
-      <h5>Project Wideawake</h5>
-      <h5>Master Mold</h5>
-      <h5>Mansion Attack</h5>
-      <h5>Magneto</h5>
+      <Link className="villainLink" to={`/villain/sabretooth`}><h5>Sabretooth</h5></Link>
+      <Link className="villainLink" to={`/villain/sentinel`}><h5>Project Wideawake</h5></Link>
+      <Link className="villainLink" to={`/villain/masterMold`}><h5>Master Mold</h5></Link>
+      <Link className="villainLink" to={`/villain/mansionAttack`}><h5>Mansion Attack</h5></Link>
+      <Link className="villainLink" to={`/villain/magneto`}><h5>Magneto</h5></Link>
     </ul>
     
     </article>;
