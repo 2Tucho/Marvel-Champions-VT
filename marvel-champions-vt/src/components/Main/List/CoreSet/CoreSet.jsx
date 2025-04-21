c4687c97845abcd28d76d7c6c4c2e67632aa176e
2Tucho <albergromero@gmail.com>
2025-04-21 10:04:44 +0200

    Added Rise of Red Skull routes

--- marvel-champions-vt/src/components/Main/List/CoreSet/CoreSet.jsx
+++ marvel-champions-vt/src/components/Main/List/CoreSet/CoreSet.jsx
@@ -7,9 +7,9 @@ function CoreSet() {
 
       <h4 className="villainEncounterName">Core Set</h4>
       <ul className="villainList">
-        <Link className="villainLink" to={`/villain/Rhyno`}><h5>Rhyno</h5></Link>
-        <Link className="villainLink" to={`/villain/Klaw`}><h5>Klaw</h5></Link>
-        <Link className="villainLink" to={`/villain/Ultron`}><h5>Ultron</h5></Link>
+        <Link className="villainLink" to={`/villain/rhyno`}><h5>Rhyno</h5></Link>
+        <Link className="villainLink" to={`/villain/klaw`}><h5>Klaw</h5></Link>
+        <Link className="villainLink" to={`/villain/ultron`}><h5>Ultron</h5></Link>
       </ul>
     
     </article>;
--- marvel-champions-vt/src/components/Main/List/TheGreenGoblin/TheGreenGoblin.jsx
+++ marvel-champions-vt/src/components/Main/List/TheGreenGoblin/TheGreenGoblin.jsx
@@ -7,8 +7,8 @@ function TheGreenGoblin() {
 
       <h4 className="villainEncounterName">The Green Goblin</h4>
       <ul className="villainList">
-        <Link className="villainLink" to={`/villain/RiskyBusiness`}><h5>Risky Business</h5></Link>
-        <Link className="villainLink" to={`/villain/MutagenFormula`}><h5>Mutagen Formula</h5></Link>
+        <Link className="villainLink" to={`/villain/riskyBusiness`}><h5>Risky Business</h5></Link>
+        <Link className="villainLink" to={`/villain/mutagenFormula`}><h5>Mutagen Formula</h5></Link>
       </ul>
     
     </article>;
