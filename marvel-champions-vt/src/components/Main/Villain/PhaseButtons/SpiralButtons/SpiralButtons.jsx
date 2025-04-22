b5fec0d4f02fb3509a003b4b1c23a647fc460508
2Tucho <albergromero@gmail.com>
2025-04-22 11:25:07 +0200

    Added logic and styles to SpiralButtons

--- marvel-champions-vt/src/components/Main/Villain/PhaseButtons/SpiralButtons/SpiralButtons.jsx
+++ marvel-champions-vt/src/components/Main/Villain/PhaseButtons/SpiralButtons/SpiralButtons.jsx
@@ -1,9 +1,29 @@
-import React, { Component } from "react";
+import React from "react";
+import {useContext} from "react";
+import { PhaseButtonsContext } from "../../../../../context/PhaseButtonsContext";
 
-class SpiralButtons extends Component {
-  render() {
-    return <div>SpiralButtons</div>;
-  }
+function SpiralButtons() { 
+  const {setPhase} = useContext(PhaseButtonsContext);
+
+  return <div id="spiralButtons">
+
+    <article>
+      <button className="phaseIA" onClick={() => setPhase("default")}>Escaped I</button> 
+      <button className="phaseIB" onClick={() => setPhase("phaseIB")}>Cornered I</button>
+    </article>
+
+    <article>
+      <button className="phaseIIA" onClick={() => setPhase("phaseIIA")}>Escaped II</button> 
+      <button className="phaseIIB" onClick={() => setPhase("phaseIIB")}>Cornered II</button>
+    </article>
+
+    <article>
+      <button className="phaseIIIA" onClick={() => setPhase("phaseIIIA")}>Escaped III</button> 
+      <button className="phaseIIIB" onClick={() => setPhase("phaseIIIB")}>Cornered III</button>
+    </article>
+
+  </div>;
+
 }
 
 export default SpiralButtons;
