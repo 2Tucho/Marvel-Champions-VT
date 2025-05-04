b873c30bf6eef804ea7a34aebef66ee1395b6655
2Tucho <albergromero@gmail.com>
2025-05-04 19:05:57 +0200

    Added new component only for the Status Tokens

---
+++ marvel-champions-vt/src/components/Main/Villain/CardImage/StatusTokens/StatusTokens.jsx
@@ -0,0 +1,23 @@
+import React from "react";
+import { useContext } from "react";
+//import { useParams } from "react-router-dom";
+
+import { StatusCardsContext } from "../../../../../context/StatusCardsContext";
+
+function StatusTokens() {
+  const { toughStatus, confusedStatus, stunnedStatus } = useContext(StatusCardsContext); /* Get the status Cars states from the context */
+
+  //const villainId = useParams(); /* Get the villainId from the URL parameters */
+
+  return <div>
+    {/* If  toughStatus / confusedStatus / stunnedStatus are true their respective element will show, if they are false then stay hide */}
+    {toughStatus && (<img id="toughToken" className="statusToken" src={"/StatusTokens/tough.jpg"} alt="Tough Token" />)}
+
+    {confusedStatus && (<img id="confusedToken" className="statusToken" src={"/StatusTokens/confused.jpg"} alt="Confused Token" />)}
+
+    {stunnedStatus && (<img id="stunnedToken" className="statusToken" src={"/StatusTokens/stunned.jpg"} alt="Stunned Token" />)}
+  </div>;
+
+}
+
+export default StatusTokens;
--- marvel-champions-vt/vite.config.js
+++ marvel-champions-vt/vite.config.js
@@ -1,15 +1,7 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
-import basicSsl from '@vitejs/plugin-basic-ssl'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), basicSsl({
-    /** name of certification */
-    name: 'test',
-    /** custom trust domains */
-    domains: ['*.custom.com'],
-    /** custom certification directory */
-    certDir: '/Users/.../.devServer/cert',
-  }),],
+  plugins: [react()],
 })
