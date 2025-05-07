59bbd9199a72f2b5d089b13d4e1fa2c6b2cf192e
2Tucho <albergromero@gmail.com>
2025-05-07 11:15:16 +0200

    Fixed Risky Business showing the Status Tokens when in Norman Osborn phase

--- marvel-champions-vt/src/components/Main/Villain/CardImage/StatusTokens/StatusTokens.jsx
+++ marvel-champions-vt/src/components/Main/Villain/CardImage/StatusTokens/StatusTokens.jsx
@@ -1,24 +1,31 @@
 import React from "react";
 import { useContext } from "react";
-//import { useParams } from "react-router-dom";
+import { useParams } from "react-router-dom";
 
 import { StatusCardsContext } from "../../../../../context/StatusCardsContext";
+import { PhaseButtonsContext } from "../../../../../context/PhaseButtonsContext";
 
 function StatusTokens() {
   const { toughStatus, confusedStatus, stunnedStatus } = useContext(StatusCardsContext); /* Get the status Cars states from the context */
+  const { phase } = useContext(PhaseButtonsContext);
 
-  //const villainId = useParams(); /* Get the villainId from the URL parameters */
+  const villainId = useParams(); /* Get the villainId from the URL parameters */
 
-  return <article id="statusTokensContainer">
-
-      {/* If  toughStatus / confusedStatus / stunnedStatus are true their respective element will show, if they are false then stay hide */}
-      {toughStatus && (<img id="toughToken" className="statusToken" src={"/StatusTokens/tough.jpg"} alt="Tough Token" />)}
-
-      {confusedStatus && (<img id="confusedToken" className="statusToken" src={"/StatusTokens/confused.jpg"} alt="Confused Token" />)}
-
-      {stunnedStatus && (<img id="stunnedToken" className="statusToken" src={"/StatusTokens/stunned.jpg"} alt="Stunned Token" />)}
-      
-    </article>;
+  return (villainId.villainId === "riskyBusiness" && !phase.includes("B") ? null : ( /* If the Villain is RiskyBusiness and not the B phase I want to hide the buttons */
+      <article id="statusTokensContainer">
+        {/* If toughStatus / confusedStatus / stunnedStatus are true, their respective element will show */}
+        {toughStatus && (
+          <img id="toughToken" className="statusToken" src={"/StatusTokens/tough.jpg"} alt="Tough Token" />
+        )}
+        {confusedStatus && (
+          <img id="confusedToken" className="statusToken" src={"/StatusTokens/confused.jpg"} alt="Confused Token" />
+        )}
+        {stunnedStatus && (
+          <img id="stunnedToken" className="statusToken" src={"/StatusTokens/stunned.jpg"} alt="Stunned Token" />
+        )}
+      </article>
+    )
+  );
 
 }
 
